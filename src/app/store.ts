defa2af7700a6699315209601a34015ea55e81f8
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-29 09:56:04 +0530

    feat: setup redux

---
+++ src/app/store.ts
@@ -0,0 +1,22 @@
+import { Action, configureStore } from "@reduxjs/toolkit"
+
+interface CounterState {
+	value: number
+}
+
+const counterReducer = (state: CounterState = { value: 0 }, action: Action) => {
+	switch(action.type) {
+		default:
+			return state
+	}
+}
+
+export const store = configureStore({
+	reducer: {
+		counter: counterReducer
+	}
+})
+
+export type AppStore = typeof store
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
