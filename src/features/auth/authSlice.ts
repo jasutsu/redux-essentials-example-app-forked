cd69590463db85aa65dfe9ab452cd0a9a4ca62d6
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-30 12:02:36 +0530

    feat: add auth slice to the redux store

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface AuthState {
+	username: string | null
+}
+
+const initialState: AuthState = {
+	username: null
+}
+
+const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {
+		userLoggedIn(state, action: PayloadAction<string>) {
+			state.username = action.payload
+		},
+		userLoggedOut(state) {
+			state.username = null
+		}
+	},
+	selectors: {
+		selectCurrentUsername: (authState) => authState.username
+	}
+})
+
+export const { userLoggedIn, userLoggedOut } = authSlice.actions
+export const { selectCurrentUsername } = authSlice.selectors
+
+export default authSlice.reducer
